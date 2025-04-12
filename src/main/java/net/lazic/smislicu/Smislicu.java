12b50841be3ff684ff51bd825207e3d7d84a7990
kuvsamja <kuv.cizal.2011@gmail.com>
2025-04-12 16:29:32 +0200

    added Birgerova Krema

--- src/main/java/net/lazic/smislicu/Smislicu.java
+++ src/main/java/net/lazic/smislicu/Smislicu.java
@@ -1,9 +1,11 @@
 package net.lazic.smislicu;
 
+import net.lazic.smislicu.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,8 +28,11 @@ public class Smislicu
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Smislicu() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/net/lazic/smislicu/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.lazic.smislicu.item;
+
+import net.lazic.smislicu.Smislicu;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.lwjgl.system.CallbackI;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Smislicu.MOD_ID);
+
+    public static final RegistryObject<Item> BIRGEROVAKREMA = ITEMS.register("birgerovakrema",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> BIRGEROVAKREMA = ITEMS.register("birgerovakrema",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
